46aaac79ccbed02109a5836563cdab96e47ef5e8
Alex Blumenthaler <blumenthaleralex@gmail.com>
2020-07-19 21:14:04 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,10 +4,7 @@ class Student
 
   @@all = []
 
-  def initialize(student_hash) # hash of collected students
-  # takes in an argument of a hash and sets that new student's attributes usingthe key/value pairs of that hash
-  #  adds that new student to the Student class' collection of all existing students, stored in the `@@all` class variable
-  
+  def initialize(student_hash)
   self.send("name=", student_hash[:name])
   self.send("location=", student_hash[:location])
     

18eac5792054a95a4bbaa7a56e2b1fb2b3decfaf
Alex Blumenthaler <blumenthaleralex@gmail.com>
2020-07-19 20:47:58 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,6 +12,7 @@ class Student
 
   def self.create_from_collection(students_array)
     # uses the Scraper class to create new students with the correct name and location
+    # iterate over hash
   end
 
   def add_student_attributes(attributes_hash)

c4594b49b22d29c964288ffbd022fa7568a62f60
Alex Blumenthaler <blumenthaleralex@gmail.com>
2020-07-19 21:27:42 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -20,7 +20,7 @@ class Student
   def add_student_attributes(attributes_hash) # Scraper.scrape_profile_page hash
     # uses the Scraper class to get a hash of a given students attributes and uses that hash to set additional attributes for that student
     # iterate over hash, meta-program to dynamically assign student attrs and values, per the key/value pairs
-    # use send, use self
+
     self.send("bio=", attributes_hash[:bio])
     self.send("twitter=", attributes_hash[:twitter])    
     self.send("linkedin=", attributes_hash[:linkedin])
